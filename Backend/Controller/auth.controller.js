491dbc38235d6062fa252982ccbd8ece2d9175dd
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-06 13:11:10 +0800

    FE - update minor change in vscode

--- Backend/Controller/auth.controller.js
+++ Backend/Controller/auth.controller.js
@@ -58,45 +58,50 @@ export const login = async (req, res, next) => {
   }
 };
 
+export const googleAuth = async (req, res, next) => {
+  try {
+    const { name, email, photo } = req.body;
 
+    const user = await User.findOne({ email });
 
-export const googleAuth = async (req , res , next) =>{
-try{
-  const {name, email , photo} = req.body
+    if (user) {
+      const accToken = jwt.sign({ id: user.id }, JWT_SECRET);
+      const { password, ...rest } = user._doc;
+      const expiryDate = new Date(Date.now() + 35000);
+      return res
+        .cookie("access_token", accToken, {
+          httpOnly: true,
+          expire: expiryDate,
+        })
+        .status(200)
+        .send(rest);
+    } else {
+      const generatePassword = Math.random().toString(36).slice(-8);
+      const hashPassword = bcryptjs.hashSync(generatePassword, 10);
+      const googleUsername =
+        name.split(" ").join("").toLowerCase() +
+        Math.floor(Math.random() * 10000);
+      const expiryDate = new Date(Date.now() + 35000);
 
-  const user = await User.findOne({email})
+      const googleUser = new User({
+        username: googleUsername,
+        email,
+        password: hashPassword,
+        profilePicture: photo,
+      });
 
-  if(user){
-    const accToken = jwt.sign({id: user.id}, JWT_SECRET);
-    const {password, ...rest} = user._doc;
-    const expiryDate = new Date(Date.now() + 35000);
-    return res.cookie("access_token" , accToken, {
-      httpOnly: true,
-      expire: expiryDate
-    }).status(200).send(rest)
-  }else{
-  
+      await googleUser.save();
+      const accToken = jwt.sign({ id: googleUser._id }, JWT_SECRET);
 
-    const generatePassword = Math.random().toString(36).slice(-8)
-    const hashPassword = bcryptjs.hashSync(generatePassword, 10)
-    const googleUsername = name.split(" ").join("").toLowerCase() + Math.floor(Math.random() * 10000)
-    const expiryDate = new Date(Date.now() + 35000)
-    
-    const googleUser = new User({
-      username: googleUsername,
-      email,
-      password: hashPassword,
-      profilePicture: photo
-    })
-
-    await googleUser.save()
-    const accToken = jwt.sign({id: googleUser._id}, JWT_SECRET)
-
-    return res.cookie("access_token" , accToken, {httpOnly:true, expire: expiryDate}).status(200).send(googleUser)
-   
+      return res
+        .cookie("access_token", accToken, {
+          httpOnly: true,
+          expire: expiryDate,
+        })
+        .status(200)
+        .send(googleUser);
+    }
+  } catch (error) {
+    return next(errorHandler(401, "Google Auth Failed"));
   }
-
-}catch(error){
-  return next(errorHandler(401 , "Google Auth Failed"))
-}
-}
+};
--- Frontend/src/Page/Profile.tsx
+++ Frontend/src/Page/Profile.tsx
@@ -1,49 +1,65 @@
-import { useSelector } from "react-redux"
-import { RootState } from "../redux/store"
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../redux/store';
 
 const Profile: React.FC = () => {
-
-    const {currentUser} = useSelector((state:RootState) => state.user)
+  const { currentUser } = useSelector((state: RootState) => state.user);
 
   return (
-   <div className="container">
-    <div className="grid justify-between gap-2">
-    <div className="shadow-lg p-5 rounded-sm">
-        <figure className="w-32 aspect-square overflow-hidden rounded-full"><img className="w-full object-cover" src={currentUser?.profilePicture} alt={currentUser?.username} /></figure>
-        <div className="mt-4">
-          <div><span>Username: </span><span>{currentUser?.username}</span></div>
-          <div><span>Email: </span><span>{currentUser?.email}</span></div>
+    <div className="container">
+      <div className="grid justify-between gap-2">
+        <div className="rounded-sm p-5 shadow-lg">
+          <figure className="aspect-square w-32 overflow-hidden rounded-full">
+            <img
+              className="w-full object-cover"
+              src={currentUser?.profilePicture}
+              alt={currentUser?.username}
+            />
+          </figure>
+          <div className="mt-4">
+            <div>
+              <span>Username: </span>
+              <span>{currentUser?.username}</span>
+            </div>
+            <div>
+              <span>Email: </span>
+              <span>{currentUser?.email}</span>
+            </div>
+          </div>
+        </div>
+        <div className="rounded-sm p-5 shadow-lg">
+          <div>
+            <h1 className="mb-3 text-center text-xl font-bold">Friends</h1>
+          </div>
+          <div className="flex flex-col gap-3">
+            <section className="flex items-center gap-3 rounded-sm p-2 shadow-sm">
+              <figure className="aspect-square w-full max-w-6 overflow-hidden rounded-full">
+                <img src={currentUser?.profilePicture} alt="" />
+              </figure>
+              <h2>{currentUser?.username}</h2>
+            </section>
+            <section className="flex items-center gap-3 rounded-sm p-2 shadow-sm">
+              <figure className="aspect-square w-full max-w-6 overflow-hidden rounded-full">
+                <img src={currentUser?.profilePicture} alt="" />
+              </figure>
+              <h2>{currentUser?.username}</h2>
+            </section>
+            <section className="flex items-center gap-3 rounded-sm p-2 shadow-sm">
+              <figure className="aspect-square w-full max-w-6 overflow-hidden rounded-full">
+                <img src={currentUser?.profilePicture} alt="" />
+              </figure>
+              <h2>{currentUser?.username}</h2>
+            </section>
+            <section className="flex items-center gap-3 rounded-sm p-2 shadow-sm">
+              <figure className="aspect-square w-full max-w-6 overflow-hidden rounded-full">
+                <img src={currentUser?.profilePicture} alt="" />
+              </figure>
+              <h2>{currentUser?.username}</h2>
+            </section>
+          </div>
         </div>
       </div>
-      <div className="shadow-lg p-5 rounded-sm">
-      <div>
-        <h1 className="text-center mb-3 text-xl font-bold">Friends</h1>
-      </div>
-      <div className="flex flex-col gap-3">
-        <section className="flex gap-3 items-center shadow-sm rounded-sm p-2">
-          <figure className="overflow-hidden max-w-6 aspect-square w-full rounded-full"><img src={currentUser?.profilePicture} alt="" /></figure>
-          <h2>{currentUser?.username}</h2>
-        </section>
-        <section className="flex gap-3 items-center shadow-sm rounded-sm p-2">
-          <figure className="overflow-hidden max-w-6 aspect-square w-full rounded-full"><img src={currentUser?.profilePicture} alt="" /></figure>
-          <h2>{currentUser?.username}</h2>
-        </section>
-        <section className="flex gap-3 items-center shadow-sm rounded-sm p-2">
-          <figure className="overflow-hidden max-w-6 aspect-square w-full rounded-full"><img src={currentUser?.profilePicture} alt="" /></figure>
-          <h2>{currentUser?.username}</h2>
-        </section>
-        <section className="flex gap-3 items-center shadow-sm rounded-sm p-2">
-          <figure className="overflow-hidden max-w-6 aspect-square w-full rounded-full"><img src={currentUser?.profilePicture} alt="" /></figure>
-          <h2>{currentUser?.username}</h2>
-        </section>
-      </div>
-      </div> 
     </div>
-   </div>
-  )
-}
+  );
+};
 
-  
-
-export default Profile
+export default Profile;
