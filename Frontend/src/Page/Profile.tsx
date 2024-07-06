8cfb99ff06ed7bda4dbdcf005cc1b4ea59245c83
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-06 14:48:03 +0800

    FE - upload picture  data to the useState

--- Frontend/src/Page/Profile.tsx
+++ Frontend/src/Page/Profile.tsx
@@ -1,18 +1,43 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
+import { useRef, useState } from 'react';
+
+import { FaFileUpload } from 'react-icons/fa';
 
 const Profile: React.FC = () => {
   const { currentUser } = useSelector((state: RootState) => state.user);
+  const imageUploadRef = useRef<HTMLInputElement>(null);
+  const [imageFile, setImageFile] = useState<File | null>();
 
+  const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      return setImageFile(e.target.files[0]);
+    }
+
+    return setImageFile(null);
+  };
   return (
     <div className="container">
       <div className="grid justify-between gap-2">
         <div className="rounded-sm p-5 shadow-lg">
-          <figure className="aspect-square w-32 overflow-hidden rounded-full">
+          <figure className="group relative aspect-square w-32 overflow-hidden rounded-full transition-all">
+            <div className="pointer-events-none absolute inset-0 m-auto flex items-center justify-center bg-gray-700 bg-opacity-0 group-hover:bg-opacity-35">
+              <FaFileUpload className="text-4xl text-white opacity-0 group-hover:opacity-100" />
+            </div>
+            <form action="">
+              <input
+                type="file"
+                ref={imageUploadRef}
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageUpload}
+              />
+            </form>
             <img
               className="w-full object-cover"
               src={currentUser?.profilePicture}
               alt={currentUser?.username}
+              onClick={() => imageUploadRef.current?.click()}
             />
           </figure>
           <div className="mt-4">
