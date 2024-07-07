91dcdeb074e352586b156786eeabeb6f866c5d97
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-07 19:20:45 +0800

    update cookie samesite to None and secure to true

--- Backend/Utils/verifyJWTToken.js
+++ Backend/Utils/verifyJWTToken.js
@@ -15,7 +15,7 @@ export const verifyJWTToken = (req, res, next) => {
     if (err) {
       res.clearCookie("access_token", {
         httpOnly: true,
-        sameSite: "lax",
+        sameSite: "None",
         secure: true,
       });
       return next(errorHandler(403, "Invalid Token"));
