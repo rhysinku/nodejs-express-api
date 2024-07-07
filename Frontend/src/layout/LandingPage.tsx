bd7702ec31d2de3cf2c881a5d80c077625317f7a
PockeyPepero <rhysinvillahermosa16@gmail.com>
2024-07-07 18:32:17 +0800

    FE - remove image from the landing page

--- Frontend/src/layout/LandingPage.tsx
+++ Frontend/src/layout/LandingPage.tsx
@@ -1,14 +1,13 @@
-import { Outlet } from "react-router-dom";
-import Navigation from "../components/Navigation";
+import { Outlet } from 'react-router-dom';
+import Navigation from '../components/Navigation';
 
 export default function LandingPage() {
   return (
     <>
       <div className="container">
-        <img src="rain-before.jpg" alt="" />
         <Navigation />
       </div>
-      <div className="h-[calc(100vh-120px)] flex items-center content-center flex-col">
+      <div className="flex h-[calc(100vh-120px)] flex-col content-center items-center">
         <div className="container h-full">
           <Outlet />
         </div>
